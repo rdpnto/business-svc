b3c46a2208c6aa86fa5d805fc05c2c96799c4d15
Rodrigo Pinto <rodrigo.souza-terceiro@cea.com.br>
2022-06-29 19:39:59 -0300

    evolved test and finished documentation

--- tests/BusinessSvc.Application.Tests/Commands/UpdateOrderStatus/UpdateOrderStatusCommandTest.cs
+++ tests/BusinessSvc.Application.Tests/Commands/UpdateOrderStatus/UpdateOrderStatusCommandTest.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using BusinessSvc.Application.Commands.UpdateOrderStatus;
+using BusinessSvc.Domain.Entities;
+using BusinessSvc.Domain.Enums;
+using MediatR;
+using Shouldly;
+using Xunit;
 
 namespace BusinessSvc.Application.Tests.Commands.UpdateOrderStatus
 {
     public class UpdateOrderStatusCommandTest
     {
+        UpdateOrderStatusCommand _command;
+
+        [Fact]
+        public void ShouldImplementMediator()
+        {
+            _command.ShouldBeAssignableTo<IRequest<UpdateOrderStatusCommandResponse>>();
+        }
+
+        [Fact]
+        public void ShouldHaveOrderProperty()
+        {
+            _command = new UpdateOrderStatusCommand(1, OrderStatus.Completed);
+
+            _command.Order.ShouldBeOfType<Order>();
+        }
     }
 }
